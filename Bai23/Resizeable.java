5bc778abc33ab0e4608bfcc70c2f4ecad17e70f5
MaiGiangNam <namquangson@gmail.com>
2023-03-15 18:16:49 +0700

    nop bai tap

---
+++ Bai23/Resizeable.java
@@ -0,0 +1,6 @@
+package Bai23;
+
+public interface Resizeable {
+    void resize(double percent);
+}
+
---
+++ Bai23/ShapeMain.java
@@ -0,0 +1,11 @@
+package Bai23;
+
+public class ShapeMain {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }
+}
